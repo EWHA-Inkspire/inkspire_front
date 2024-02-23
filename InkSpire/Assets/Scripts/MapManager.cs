262c61335046365704417af8b5d1041128eb717a
Gabin Lee <gb94@ewhain.net>
2024-02-23 17:47:27 +0900

    Feat: 아이템 생성 GPT 호출 함수

--- InkSpire/Assets/Scripts/MapManager.cs
+++ InkSpire/Assets/Scripts/MapManager.cs
@@ -3,17 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using OpenAI;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class MapManager : MonoBehaviour
 {
-    private ScriptManager scriptManager;
     public static MapManager mapinfo;
     public struct place
     {
         public string place_name; //장소 이름
         public string place_info; //장소 설명
-        public string item_name; //아이템 이름, 아이템 설명? 
+        public string item_name; //아이템 이름 
+        public string item_info; //아이템 설명
         public string item_type; //일반 이벤트일 경우 아이템, 목표 이벤트일 경우 목표
+        public string event_trigger; //이벤트 트리거
         public int event_type; //일반 이벤트 == 0, 목표 이벤트 == 1;
         public int ANPC_exist; //ANPC 등장 여부
     }
@@ -64,13 +66,13 @@ public class MapManager : MonoBehaviour
         IsANPCexists();
         ChooseItemType();
     }
-    public void setBackground(string time, string space, string gen)
-    {
-        time_background = time;
-        space_background = space;
-        genre = gen;
-        CreatePlace();
-    }
+    // public void setBackground(string time, string space, string gen)
+    // {
+    //     time_background = time;
+    //     space_background = space;
+    //     genre = gen;
+    //     CreatePlace();
+    // }
 
     //목표 or 일반 이벤트 여부 정하기
     public void ChooseEventType()
@@ -110,48 +112,33 @@ public class MapManager : MonoBehaviour
             map[i].ANPC_exist = 0;
         }
     }
-    private async void CreateItem()
+    private async void CreateItem(int place_idx)
     {
         Debug.Log(">>Call Create ITEM GPT");
         Debug.Log(">>현재 장소 인덱스: " + place_idx);
         gpt_messages.Clear();
-
-        if (place_idx == 0) //장소 인덱스가 0일 경우 PNPC 장소 생성
+        string about_item = "";
+        if (map[place_idx].item_type == "Recover")
+            about_item = "플레이어의 HP를 회복시켜주는 아이템";
+        else if (map[place_idx].item_type == "Mob")
+            about_item = "잡몹을 처치했을 때 얻는 보상으로 여러 개를 모았을 때 힘이 발휘되는 아이템"; //아니 드래곤볼 어케 설명하지..ㅠ
+        else if (map[place_idx].item_type == "Weapon")
+            about_item = "플레이어의 공격력을 높여주는 무기 아이템";
+        map[place_idx].item_info = about_item;
+        var prompt_msg = new ChatMessage()
         {
-            var prompt_msg = new ChatMessage()
-            {
-                Role = "system",
-                Content = @"당신은 조력자 NPC가 머무는 장소를 제시한다.
+            Role = "system",
+            Content = @"당신은 게임 진행에 필요한 아이템의 이름을 한 단어로 제시한다.
             다음은 게임의 배경인 
             " + time_background + "시대" + space_background + "를 배경으로 하는 세계관에 대한 설명이다." + world_detail +
-                @"장소는 해당 게임의 조력자 NPC의 집 혹은 직장으로 생성되며 조력자 NPC의 정보는 다음과 같다." + PNPC_info + //이거 변수 어디서 가져오는지 확인
-                @"장소 생성 양식은 아래와 같다. 각 줄의 요소는 반드시 모두 포함되어야 하며, 답변할 때 줄바꿈을 절대 하지 않는다. ** 이 표시 안의 내용은 문맥에 맞게 채운다.
-
-
-            장소명: *장소 이름을 한 단어로 출력*
-            장소설명: *장소에 대한 설명을 50자 내외로 설명 * "
-            };
-        }
-        else //장소 인덱스가 0이 아닐 경우 일반 장소 생성
-        {
-            var prompt_msg = new ChatMessage()
-            {
-                Role = "system",
-                Content = @"당신은 게임 진행에 필요한 장소를 제시한다.
-            다음은 게임의 배경인 
-            " + time_background + "시대" + space_background + "를 배경으로 하는 세계관에 대한 설명이다." + world_detail +
-                @"장소는 게임의 배경에 맞추어 플레이어가 흥미롭게 탐색할 수 있는 곳으로 생성된다. 장소 생성 양식은 아래와 같다. 각 줄의 요소는 반드시 모두 포함되어야 하며, 답변할 때 줄바꿈을 절대 하지 않는다. ** 이 표시 안의 내용은 문맥에 맞게 채운다.
-            
-            장소명: *장소 이름을 한 단어로 출력*
-            장소설명: *장소에 대한 설명을 50자 내외로 설명*"
-            };
-        }
+            @"당신이 생성해야할 아이템은" + about_item + "이다."
+        };
         gpt_messages.Add(prompt_msg);
 
         var query_msg = new ChatMessage()
         {
             Role = "user",
-            Content = "진행중인 게임의 " + genre + "장르와 세계관에 어울리는 장소 생성"
+            Content = "진행중인 게임에 필요한" + about_item + "생성"
         };
         gpt_messages.Add(query_msg);
 
@@ -167,7 +154,7 @@ public class MapManager : MonoBehaviour
             var message = completionResponse.Choices[0].Message;
             message.Content = message.Content.Trim();
             Debug.Log(message.Content);
-            map[place_idx] = StringToPlace(message.Content);
+            map[place_idx].item_name = message.Content;
             curr_place = place_idx; //curr_chapter가 어디에서 i++되는 변수인지 확인하기
             if (curr_place != 1)
             {
@@ -301,8 +288,6 @@ public class MapManager : MonoBehaviour
         {
             Debug.LogWarning("No text was generated from this prompt.");
         }
-
-
     }
 
     //장소 이름 및 장소 설명 파싱 함수
