8db53680e2c4534b8939419057bd5ca20ca4faa6
Gabin Lee <gb94@ewhain.net>
2024-04-11 22:58:15 +0900

    Feat: 탐험 목록 챕터 리스트 스크립트

--- InkSpire/Assets/Scripts/ChapterList.cs
+++ InkSpire/Assets/Scripts/ChapterList.cs
@@ -2,14 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 
 public class ChapterList : MonoBehaviour
 {
-    string characterName = GetComponentInChildren<Text>().text;
+    [SerializeField] GameObject Chapter1;
+    [SerializeField] GameObject Chapter2;
+    [SerializeField] GameObject Chapter3;
+    [SerializeField] GameObject Chapter4;
+    [SerializeField] GameObject Chapter5;
+    [SerializeField] TextMeshProUGUI ModalTitle;
+    string characterName;
     void SetTitle()
     {
+        characterName = GetComponentInChildren<Text>().text;
         ModalTitle.text = characterName + " 's Story";
+    }
+
+    void activateChapter()
+    {
+        Chapter1.gameObject.SetActive(true);
+        if (ScriptManager.scriptinfo.curr_chapter >= 2)
+            Chapter2.gameObject.SetActive(true);
+        else if (ScriptManager.scriptinfo.curr_chapter >= 3)
+            Chapter3.gameObject.SetActive(true);
+        else if (ScriptManager.scriptinfo.curr_chapter >= 4)
+            Chapter4.gameObject.SetActive(true);
+        else
+            Chapter5.gameObject.SetActive(true);
     }
 }
