eafd5820976c0789077245dafe707303e6dfe6c5
Gabin Lee <gb94@ewhain.net>
2024-05-22 05:47:32 +0900

    Feat: 맵 버튼 방문 장소 및 현재 장소 색상 변경

--- InkSpire/Assets/Scripts/UI/ButtonTextChange.cs
+++ InkSpire/Assets/Scripts/UI/ButtonTextChange.cs
@@ -4,34 +4,41 @@ using TMPro;
 
 public class ButtonTextChange : MonoBehaviour
 {
-    public TextMeshProUGUI buttonText;
-    public Button button;
-    Color newColor;
-    string newColor_code;
+    private TextMeshProUGUI[] buttonTexts;
+    private Button[] buttons;
+    private Button lastClickedButton = null;
+    private Color clickedTextColor = Color.gray;
+    private Color redColor = Color.red; // 빨간색
+
     void Start()
     {
-        if (button == null)
+        buttons = GetComponentsInChildren<Button>();
+        buttonTexts = GetComponentsInChildren<TextMeshProUGUI>();
+
+        foreach (Button button in buttons)
         {
-            Debug.LogError("Button reference is not set. Please assign the button in the Inspector.");
-            enabled = false; // 버튼이 없으면 스크립트를 비활성화
-            return;
+            button.onClick.AddListener(() => OnClickButton_ChangeColor(button));
         }
 
-        button.onClick.AddListener(OnClickButton_ChangeColor);
-        buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+        // 초기 버튼의 텍스트 색상 설정
+        foreach (TextMeshProUGUI buttonText in buttonTexts)
+        {
+            buttonText.color = Color.black;
+        }
     }
-    public void OnClickButton_ChangeColor()
+
+    public void OnClickButton_ChangeColor(Button clickedButton)
     {
-        newColor_code = "#B0B0B0"; //연회색 (빨간색은 #C53D3D)
-        if (buttonText == null)
+        // 이전에 클릭된 버튼의 텍스트 색상을 회색으로 변경
+        if (lastClickedButton != null)
         {
-            Debug.LogError("Text reference is not set. Please assign the text in the Inspector.");
-            enabled = false; // 텍스트가 없으면 스크립트를 비활성화
-            return;
+            lastClickedButton.GetComponentInChildren<TextMeshProUGUI>().color = clickedTextColor;
         }
-        if (ColorUtility.TryParseHtmlString(newColor_code, out newColor))
-        {
-            buttonText.color = newColor;
-        }
+
+        // 현재 클릭된 버튼을 저장하여 이후에 다시 회색으로 변경할 수 있도록 함
+        lastClickedButton = clickedButton;
+
+        // 가장 최근에 클릭된 버튼의 텍스트 색상을 빨간색으로 변경
+        clickedButton.GetComponentInChildren<TextMeshProUGUI>().color = redColor;
     }
 }
