9cdfe40ab85392301629f4d23b957894777ac878
OSSU <o.ss.u2056@gmail.com>
2024-05-27 14:12:27 +0900

    fix: 장소 개수 챕터수에 따라 조정되도록 수정

--- InkSpire/Assets/Scripts/Data/Const.cs
+++ InkSpire/Assets/Scripts/Data/Const.cs
@@ -2,5 +2,5 @@ public static class Const
 {
     public static string BASE_URL = "http://localhost:8080"; // localhost 부분 서버 IP로 변경해서 사용할 것!!
     public static int CHAPTER = 3;
-    public static int PLACE_COUNT = 11;
+    public static int PLACE_COUNT = (CHAPTER - 1) * 3 + 2;
 }
